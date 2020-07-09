0c181a8724bdb29c0b7014636219c98a02609136
ghdtjr <ghdtjr0311@gmail.com>
2020-07-09 21:51:13 +0900

    feat: tab structure with viewpager sych

--- app/src/main/java/com/example/tab_application/MainActivity.java
+++ app/src/main/java/com/example/tab_application/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.tab_application;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager vp = findViewById(R.id.viewpager);
+        VPAdapter adapter = new VPAdapter(getSupportFragmentManager());
+        vp.setAdapter(adapter);
+
+        // sync between tab and viewpager
+        TabLayout tab = findViewById(R.id.tabs);
+        tab.setupWithViewPager(vp);
+//        // add images on the tab
+//        ArrayList<Integer> images = new ArrayList<>();
+//        images.add(R.drawable.cal);
+//        images.add(R.drawable.sea);
+//        images.add(R.drawable.set);
+//        for(int i=0; i<3; i++) tab.getTabAt(i).setIcon
     }
 }
